26ed3830aba53e416f555539640c401c0574ecc5
pedroLopes5555 <pedrosilvestrelopes@hotmail.com>
2025-11-19 22:48:25 +0000

    first blood

---
+++ backend/be-atec/src/main/java/atec/beatec/BeAtecApplication.java
@@ -0,0 +1,13 @@
+package atec.beatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAtecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAtecApplication.class, args);
+    }
+
+}
---
+++ backend/be-atec/src/test/java/atec/beatec/BeAtecApplicationTests.java
@@ -0,0 +1,13 @@
+package atec.beatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAtecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
