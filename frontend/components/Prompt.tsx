03bd216e34919ddfbc06b9a0a3fbfeac05342ef9
Iúri Neves <iuriayolphineves@gmail.com>
2026-01-02 20:56:36 +0000

    feat: add pwa support

---
+++ frontend/components/Prompt.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import PromptSteps from "./PromptSteps";
+export default function Prompt() {
+  const [isStandalone, setIsStandalone] = useState(true);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
+  }, []);
+  return (
+    !isStandalone &&
+    process.env.NEXT_PUBLIC_DEBUG == "false" && (
+      <>
+        {open && <PromptSteps setOpen={setOpen} />}
+        <div className="md:hidden fixed w-full flex justify-between top-0 border-b border-secondary/25 h-18 p-4 gap-2 font-sans bg-background">
+          <div className="flex gap-2 items-center">
+            <div className="h-full aspect-square p-2 border border-secondary/25 rounded-xl mt-1">
+              <Image
+                src={"/atec_logo.png"}
+                width={1000}
+                height={1000}
+                alt="atec logo"
+              />
+            </div>
+            <div>
+              <div className="font-bold">ATEC</div>
+              <div className="text-secondary text-sm">
+                Instala a app no telemóvel
+              </div>
+            </div>
+          </div>
+          <button
+            className="bg-primary text-background px-4 rounded-md"
+            onClick={() => setOpen((open) => !open)}
+          >
+            Instalar
+          </button>
+        </div>
+      </>
+    )
+  );
+}
---
+++ frontend/components/PromptSteps.tsx
@@ -0,0 +1,41 @@
+import {
+  IconShare2,
+  IconSquarePlus,
+  IconDotsCircleHorizontal,
+} from "@tabler/icons-react";
+
+export default function PromptSteps({
+  setOpen,
+}: {
+  setOpen: (open: boolean) => void;
+}) {
+  return (
+    <div
+      className="fixed z-10 w-screen bottom-0 h-screen bg-secondary/25 backdrop-blur-xs flex flex-col-reverse p-4 font-sans"
+      onClick={() => setOpen(false)}
+    >
+      <div className="flex flex-col bottom-0 gap-5 bg-background p-4 rounded-[14px]">
+        <ol className="inline items-center">
+          1. Clica em
+          <IconDotsCircleHorizontal className="inline-flex font-bold mx-1 items-center text-center justify-center" />
+          no canto inferior direito
+        </ol>
+        <ol className="flex">
+          2. Clica em
+          <IconShare2 className="inline-flex font-bold mx-1 items-center text-center justify-center" />
+          <span className="font-bold">Partilhar</span>
+        </ol>
+        <ol className="inline items-center">
+          3. Clica em
+          <IconDotsCircleHorizontal className="inline-flex font-bold mx-1 items-center text-center justify-center" />
+          <span className="font-bold">Mais</span> no canto inferior direito
+        </ol>
+        <ol className="inline items-center">
+          4. Clica em
+          <IconSquarePlus className="inline-flex font-bold mx-1 items-center text-center justify-center" />
+          <span className="font-bold">Adicionar ao ecrã principal</span>
+        </ol>
+      </div>
+    </div>
+  );
+}
