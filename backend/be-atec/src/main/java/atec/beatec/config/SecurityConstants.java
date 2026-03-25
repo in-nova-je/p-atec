c4517f5955f4e2d29a418b18bd22a6bbedad7bd2
MIguelMestre1 <migueltmestre@gmail.com>
2026-03-25 14:26:56 +0000

    jwt expirity updated

--- backend/be-atec/src/main/java/atec/beatec/config/SecurityConstants.java
+++ backend/be-atec/src/main/java/atec/beatec/config/SecurityConstants.java
@@ -1,5 +1,5 @@
 package atec.beatec.config;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION_TIME = 172800;
+    public static final long JWT_EXPIRATION_TIME = 1728000000;
 }
